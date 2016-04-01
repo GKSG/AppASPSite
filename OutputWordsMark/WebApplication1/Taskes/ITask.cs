e0a304b6ef5774f8c95402dfeef6b661c8ef7394
KuchmyndaMarkiian <kuchmynda95@gmail.com>
2016-04-01 22:02:14 +0300

    Таск генератор SingleWord

---
+++ OutputWordsMark/WebApplication1/Taskes/ITask.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1.Taskes
+{
+    enum LevelUA
+    {
+        Beginner,Intermediate,Pro
+    }
+
+    internal interface ITask
+    {
+        string Header { get; set; }
+        string Sence { get; set; }
+        LevelUA Level { get; set; }
+        int PointsOfTask { get; set; }
+    }
+
+    class SingleTask : ITask
+    {
+        public string Header { get; set; }
+        public string Sence { get; set; }
+        public LevelUA Level { get; set; } = LevelUA.Beginner;
+        public int PointsOfTask { get; set; } = 100;
+        public string CorrectWord { get; set; }
+        public object Content { get; set; }
+    }
+
+    class SingleWord : SingleTask
+    {                      
+        public SingleWord() : base()
+        {
+            Content = "";
+            Header = "Translate word";
+            Sence = "It is necessary to practice memory and vocabulary.";
+        }
+    }
+    class SingleImage : SingleTask
+    {                                                
+        public SingleImage() : base()
+        {
+            Content = new Image();
+            Header = "What`s it?";
+            Sence = "It is necessary to practice memory and vocabulary.";
+        }
+    }
+}
